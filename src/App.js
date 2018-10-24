4f3b55f1cdaf60ed28e22a2cab3bac821d41f5b8
George <divanhost@gmail.com>
2018-10-24 10:49:30 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Add } from './components/Add'
+import { News } from './components/News'
+//import newsData from './data/newsData'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    
+class App extends React.Component{
+  state = {
+    headerText:'SampleText',
+  }
+
+  aFunc = (e) => { this.setState({headerText:e})};
+ 
+  render(){
+    const {headerText} = this.state;
+    return(
+      <React.Fragment>
+        <C text= {headerText}/>
+        <B bbFunc = {this.aFunc}/>
+      </React.Fragment>
+    );
+  }
+}
+class B extends React.Component{
+  state = {
+    headerText:'Hello, its me',
+  }
+  bFunc = (e) => { this.setState({headerText:e})};
+  render(){
+    return(
+      <div>
+        <button onClick={()=>{this.props.bbFunc(this.state.headerText)}}>Вторая</button>
+        <D dFunc ={this.bFunc}/>
       </div>
+      
     );
   }
 }
 
+class D extends React.Component{
+ componentDidMount(){
+  var text = 'Ah, Hello there';
+  this.props.dFunc(text);
+ }
+  render(){
+      return(<div></div>)
+  }
+}
+
+class C extends React.Component{
+ 
+  render(){
+    
+    return(
+      <div>
+        <h1>{this.props.text}</h1>
+      </div>
+    );
+  }
+}
 export default App;
